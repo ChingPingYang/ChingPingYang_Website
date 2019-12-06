3fc1b9fc1d83ea1371273f6da046be54365e7d14
JamesYang <gn00999829@gmail.com>
2019-12-05 16:19:19 -0800

    added more animation

--- src/components/main/slider/SliderContent.js
+++ src/components/main/slider/SliderContent.js
@@ -1,31 +1,64 @@
 import React from "react";
+import { Tween } from 'react-gsap';
+import { Controller, Scene } from "react-scrollmagic";
 
 const SliderContent = (props) => {
     const { slidesArray, currentSlide } = props;
-    return (       
+    return (
+        <Controller>
+            <Scene
+                triggerElement= "#main"
+                triggerHook= "0.7"
+                classToggle= {['.sliderContent_container', 'animateIn']}
+            >
         <div className="sliderContent_container">
             { 
                 slidesArray.map(slide => {
                     if(slide.id === currentSlide) {
                         return (
-                            <div className="sliderContent_mapDiv_container" key={slide.id}>
-                                <div className="sliderContent_titleContainer">
-                                    <h3 className="sliderContent_subtitle">{slide.subtitle}</h3>
-                                    <h1 className="sliderContent_title">{slide.title}</h1>
+                            <Tween 
+                            from={{ opacity: 0 }} 
+                            duration={2}
+                            ease="easeInOut"
+                            key={slide.id}
+                            >
+                                <div className="sliderContent_mapDiv_container">
+                                <Controller>
+                                    <Scene
+                                        triggerElement= "#main"
+                                        triggerHook= "0.7"
+                                        classToggle= {['.sliderContent_titleContainer', 'animateIn']}
+                                    >
+                                        <div className="sliderContent_titleContainer">
+                                            <h3 className="sliderContent_subtitle">{slide.subtitle}</h3>
+                                            <h1 className="sliderContent_title">{slide.title}</h1>
+                                        </div>
+                                    </Scene>
+
+                                        <div className="sliderContent_decoration_yellow"></div>
+
+                                    <Scene
+                                        triggerElement= "#main"
+                                        triggerHook= "0.7"
+                                        classToggle= {['.sliderContent_description', 'animateIn']}
+                                    >
+                                        <p className="sliderContent_description">{slide.description}</p>
+                                    </Scene>
+
+                                    <div className="sliderContent_decoration_white"></div>
+                                    <h3 className="sliderContent_roles">Roles: {slide.roles}</h3>
+                                    <div className="sliderContent_btn"><a href={slide.website_url} target="_blank">VIEV</a></div>
+                                    <div className="sliderContent_numberBG" style={{backgroundImage: `url(${slide.number_url})`}}></div>
+                                </Controller>
                                 </div>
-                                <div className="sliderContent_decoration_yellow"></div>
-                                <p className="sliderContent_description">{slide.description}</p>
-                                <div className="sliderContent_decoration_white"></div>
-                                <h3 className="sliderContent_roles">Roles: {slide.roles}</h3>
-                                <div className="sliderContent_btn"><a href={slide.website_url} target="_blank">VIEV</a></div>
-                                <div className="sliderContent_numberBG" style={{backgroundImage: `url(${slide.number_url})`}}></div>
-                            </div>
+                            </Tween>
                         )
                     }
                 })
-            
             }
         </div>
+            </Scene>
+        </Controller>
     )
 }
 
--- src/components/main/slider/SliderPicture.js
+++ src/components/main/slider/SliderPicture.js
@@ -1,28 +1,35 @@
 import React from "react";
-
+import { Controller, Scene } from "react-scrollmagic";
 
 const SliderPicture = (props) => {
     const { slidesArray, currentSlide } = props;
     
     return(
-        <div className="sliderPicture_container">
-            <div className="sliderPicture_rowOfPictures" style={{transform: `translateX(-${currentSlide * 100}%)`}}>
-                {
-                    slidesArray.map((slide, index) => {
-                        console.log(slide.url)
-                        return (
-                            <div key={index} 
-                                 className={ `${slide.id === currentSlide ? "sliderPicture_picture currentSlide" : "sliderPicture_picture"}` } 
-                                 style={{backgroundImage: `url(${slide.url})`}}>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="sliderPicture_picture"></div>
-            <div className="sliderPicture_mask top"></div>
-            <div className="sliderPicture_mask right"></div>
-        </div>
+        <Controller>
+            <Scene
+                triggerElement= "#main"
+                triggerHook= "0.7"
+                classToggle= {['.sliderPicture_container', 'animateIn']}
+            >
+                <div className="sliderPicture_container">
+                    <div className="sliderPicture_rowOfPictures" style={{transform: `translateX(-${currentSlide * 100}%)`}}>
+                        {
+                            slidesArray.map((slide, index) => {
+                                console.log(slide.url)
+                                return (
+                                    <div key={index} 
+                                        className={ `${slide.id === currentSlide ? "sliderPicture_picture currentSlide" : "sliderPicture_picture"}` } 
+                                        style={{backgroundImage: `url(${slide.url})`}}>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="sliderPicture_mask top"></div>
+                    <div className="sliderPicture_mask right"></div>
+                </div>
+            </Scene>
+        </Controller>
     )
 }
 
