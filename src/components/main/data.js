a319736074d404e0ddfc4647da8b3a6552527d4d
JamesYang <gn00999829@gmail.com>
2020-02-13 18:19:32 -0800

    added one more project

--- src/components/main/data.js
+++ src/components/main/data.js
@@ -1,6 +1,7 @@
 import project1_bg from "./images/project1_bg.jpg";
 import project2_bg from "./images/project2_bg.jpg";
 import project3_bg from "./images/project3_bg.jpg";
+import project4_bg from "./images/project4_bg.jpg";
 import project1_number from "./images/project1_number.svg"
 import project2_number from "./images/project2_number.svg"
 import project3_number from "./images/project3_number.svg"
@@ -17,7 +18,7 @@ export const data = {
         subtitle: "React.js / HTML / CSS",
         title: "BRICKHOUSE VENTURES",
         description: `This website is built for a Vancouver based company called Brickhouse Ventures.
-                      In this project I developed the About and Partners sections according to the client's needs. I also implemented GSAP and Scrollmagic
+                      In this project, I developed the About and Partners sections according to the client's needs. I also implemented GSAP and Scrollmagic
                       libraries to create fade-in animation.`,
         roles: "Developer, Animation"                
     },
@@ -25,13 +26,12 @@ export const data = {
         id: 1,
         url: project2_bg,
         number_url: project2_number,
-        website_url: "https://eager-volhard-bd9102.netlify.com",
-        subtitle: "HTML / CSS",
-        title: "GONG CHA",
-        description: `GONG CHA is a school project which was done by a small group of people (three people including me) in 4 days. 
-                      The instruction was to make a mockup website for whichever existing brand with HTML and CSS, and no JavaScript is allowed. 
-                      In this project I played two roles, designer and developer. I had one day to design the entire website and the rest of 
-                      days to develop the Hero and Feature sections.`,
+        website_url: "https://riddles-app-james.herokuapp.com/",
+        subtitle: "Node.js / Express.js / MongoDB Atlas / EJS / Sass",
+        title: "Riddles!",
+        description: `Riddles is a full-stack web app built on Node.js. We use EJS template to render the view and store data in MongoDB Atlas. 
+                      In this project, I was in charge of the whole design, front-end/back-end of the landing page, the agree/disagree functions 
+                      of the comment section, and finally the deployment by using Heroku.`,
         roles: "Developer, UI Design, Animation"                
     },
     {
@@ -39,12 +39,25 @@ export const data = {
         url: project3_bg,
         number_url: project3_number,
         website_url: "https://nostalgic-joliot-05ad64.netlify.com",
+        subtitle: "HTML / CSS",
+        title: "GONG CHA",
+        description: `GONG CHA is a school project which was done by a small group of people (three people including me) in 4 days. 
+                      The instruction was to make a mockup website for whichever existing brand with HTML and CSS, and no JavaScript is allowed. 
+                      In this project, I played two roles, designer and developer. I had one day to design the entire website and the rest of the
+                      days to develop the Hero and Feature sections.`,
+        roles: "Developer, UI Design, Animation"               
+    },
+    {
+        id: 3,
+        url: project4_bg,
+        number_url: project4_number,
+        website_url: "https://nostalgic-joliot-05ad64.netlify.com",
         subtitle: "JavaScript / CSS / HTML",
         title: "HIDING FROM BALLS",
-        description: `A small game I made for practicing logic in JavaScript. The game starts with a small green ball which 
-                      the player can move around in the game board. In the meantime there will be few other balls moving randomly in the game 
-                      area to try to kill the player. Random balls will increase as time goes, but the player also has chance to deny the auto-generated
-                      balls when it turns red.`,
+        description: `A small game I made for practicing logic in JavaScript. The game starts with a small green ball in which the player can move 
+                      around on the game board. In the meantime, there will be few other balls moving randomly in the game area to try to kill the 
+                      player. Random balls will increase as time goes, but the player also has the chance to deny the auto-generated balls when it 
+                      turns red.`,
         roles: "Developer, Animation"                
     }
     ]
