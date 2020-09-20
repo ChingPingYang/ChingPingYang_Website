a89de47af045f654e702bbd26ccc44fd9843f1af
JamesYang <gn00999829@gmail.com>
2020-09-20 14:50:49 -0700

    edited content

--- src/components/header/Content.js
+++ src/components/header/Content.js
@@ -10,12 +10,12 @@ const Content = () => {
     return (
         <div className="content_container">
             <div className="content_mask">
-            <h2 id="subtitle" className="content_subtitle">Front-End Developer / UI & Visual Designer</h2>
+            <h2 id="subtitle" className="content_subtitle">Full-Stack Developer / UI & Visual Designer</h2>
             <h1 className="content_myName">CHING PING YANG</h1>   
             <div className="content_decoration"></div>
             <p className="content_about">Hi! Welcome to my website. I’m Ching-Ping Yang, you 
             can also call me James. Passionate about creating beautiful design content and implementing 
-            into real-life websites. Currently, studying front-end development in Vancouver.</p>
+            into real-life websites. Currently working in Vancouver.</p>
             <h3 className="content_specialties">SPECIALTIES</h3>
             <div className="content_specialties_logos">
                 <img className="content_specialties_logos_react" src={react_logo} alt="React"/>
@@ -24,7 +24,7 @@ const Content = () => {
                 <img className="content_specialties_logos_css" src={Sass_logo} alt="CSS"/>
             </div>
             <h3 className="content_more">MORE</h3>
-            <h4 className="content_more_skills">jQuery, PHP, WordPress, Express, RESTful API, GraphQL, MongoDB, MySQL, JAVA, Adobe XD, Figma, Illustrator, 
+            <h4 className="content_more_skills">Express, RESTful API, GraphQL, MongoDB, jQuery, PHP, WordPress, MySQL, JAVA, Adobe XD, Figma, Illustrator, 
             Photoshop, After Effect</h4>            
             </div>
         </div>
