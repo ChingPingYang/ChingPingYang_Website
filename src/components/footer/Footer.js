b153a56a4c09b5f930bac2bd4b2d764a67cf44b6
ChingPingYang <gn00999829@gmail.com>
2026-01-31 17:11:26 -0800

    update dependencies.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,6 @@ const Footer = () => {
         <div className="footer_container">
             <div className="footer_icons_container">
                 <a href="mailto: gn00999829@gmail.com" target="_blank"><img src={email_icon} alt="ching-ping yang email" width="28px"/></a>
-                <a href="https://www.facebook.com/james.yang.397" target="_blank"><img src={facebook_icon} alt="ching-ping yang facebook" width="28px"/></a>
-                <a href="https://www.instagram.com/as00999829/" target="_blank"><img src={instagram_icon} alt="ching-ping yang instagram" width="28px"/></a>
             </div>
             <h3 className="footer_copyright">Copyright © {new Date().getFullYear()} Ching-Ping Yang. All rights reserved.</h3>
         </div>
